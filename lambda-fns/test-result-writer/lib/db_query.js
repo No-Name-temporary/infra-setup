0d53dc409f43f74e759937b196f93ffc180cc69c
mabbason <miles.abbason@gmail.com>
2022-08-06 14:53:58 -0400

    permissions success home+remote, full loop no DB
    Co-authored-by: Katarina Rosiak <katarinarosiak@gmail.com>
    Co-authored-by: Scott Graham <scttgrhm7+public@gmail.com>
    Co-authored-by: Tim Dronkers <tdronkers@gmail.com>

--- lambda-fns/test-result-writer/lib/db_query.js
+++ lambda-fns/test-result-writer/lib/db_query.js
@@ -1,15 +1,5 @@
 const { dbQuery } = require('./db_conn');
 
-async function getTestByName({ testName }) {
-  const query = `
-    SELECT * 
-    FROM tests
-    WHERE name = $1
-  `;
-  const result = await dbQuery(query, testName);
-  return result.rows[0];
-}
-
 async function getRegionByAWSName({ awsRegionName }) {
   const query = `
     SELECT * 
@@ -20,45 +10,52 @@ async function getRegionByAWSName({ awsRegionName }) {
   return result.rows[0];
 }
 
-async function getAssertionsByTestId({ testId }) {
+async function insertTestRunData({
+  testId,
+  success,
+  regionId,
+  responseStatus,
+  responseTime,
+  responseBody,
+  responseHeaders,
+}) {
   const query = `
-  SELECT * 
-  FROM assertions
-  WHERE test_id = $1
-`;
-  const result = await dbQuery(query, testId);
-  return result.rows;
-}
-
-async function insertTestRunData({ testId, regionId }) {
-  const query = `
-    INSERT INTO test_runs (test_id, started_at, completed_at, region_id)
-    VALUES ($1, $2, $3, $4)
+    INSERT INTO test_runs (test_id, success, started_at, completed_at, region_id, response_status, response_time, response_body, response_headers)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
     RETURNING id
   `;
 
   const now = new Date();
-  const result = await dbQuery(query, testId, now, now, regionId);
+  const result = await dbQuery(
+    query,
+    testId,
+    success,
+    now,
+    now,
+    regionId,
+    responseStatus,
+    responseTime,
+    responseBody,
+    responseHeaders,
+  );
   return result;
 }
 
 async function insertAssertionResultData({
-  testRunId, assertionId, actualValue, pass,
+  testRunId, assertionId, actualValue, success,
 }) {
   const query = `
-    INSERT INTO assertion_results (test_run_id, assertion_id, actual_value, pass)
+    INSERT INTO assertion_results (test_run_id, assertion_id, actual_value, success)
     VALUES ($1, $2, $3, $4)
     RETURNING id
   `;
 
-  const result = await dbQuery(query, testRunId, assertionId, actualValue, pass);
+  const result = await dbQuery(query, testRunId, assertionId, actualValue, success);
   return result;
 }
 
 module.exports = {
-  getTestByName,
   getRegionByAWSName,
-  getAssertionsByTestId,
   insertTestRunData,
   insertAssertionResultData,
 };
