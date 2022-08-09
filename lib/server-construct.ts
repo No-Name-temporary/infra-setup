8886a23f2aa4c0373edad23a61233aeba1fbe91a
mabbason <miles.abbason@gmail.com>
2022-08-08 20:03:31 -0400

    added lambda node_modules folders, updated README
    Co-authored-by: Katarina Rosiak <katarinarosiak@gmail.com>
    Co-authored-by: Tim Dronkers <tdronkers@gmail.com>
    Co-authored-by: Scott Graham <scttgrhm7+public@gmail.com>
    Co-authored-by: Miles Abbason <miles.abbason@gmail.com>

--- lib/server-construct.ts
+++ lib/server-construct.ts
@@ -43,15 +43,6 @@ export class ServerApp extends Construct {
 
     const allowAllEventBridgePolicy = iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonEventBridgeFullAccess')
     this.ebInstanceRole.addManagedPolicy(allowAllEventBridgePolicy);
-    
-
-    // this.ebInstanceRole.addToPrincipalPolicy(
-    //   new iam.PolicyStatement({
-    //     effect: iam.Effect.ALLOW,
-    //     actions: ['s3:GetObject'],
-    //     resources: [  `${this.frontendS3Bucket.bucketArn}/*` ],
-    //   }),
-    // );
 
 
     const profileName = `${appName}-InstanceProfile`
